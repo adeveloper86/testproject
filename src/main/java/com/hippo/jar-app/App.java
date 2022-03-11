9bcd076a8ca2a18e9ea826b65a3ecd2d4c50dc7e
adeveloper86 <adeveloper86@gmail.com>
2022-03-11 10:29:28 +0000

    Initial Commit

---
+++ src/main/java/com/hippo/jar-app/App.java
@@ -0,0 +1,13 @@
+package com.hippo.jar-app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
